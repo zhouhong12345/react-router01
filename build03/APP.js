780a7ee46d514c9e84ba92c74dc5faa8179f09cd
zhouhong <q15230189155@163.com>
2019-11-14 22:01:06 +0800

    11/14 new

---
+++ build03/APP.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import AppTab from './contain/AppTab'
+
+export default class APP extends Component {
+    render() {
+        return (
+            <div>
+                <AppTab/>
+            </div>
+        )
+    }
+}
